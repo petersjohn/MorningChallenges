969074dbcb57578f79bbf9245cdb010d4a40f894
John Peters <petersjohn0109@gmail.com>
2021-06-01 19:22:28 -0400

    initial Commit

---
+++ W1D3/W1D3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace W1D3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Supercalifragilisticexpialidocious";
+            int i = 0;
+            foreach (char letter in name)
+            {
+                Console.WriteLine(letter);
+            }
+            
+
+            foreach(char letter in name)
+             {
+                 if (letter != 'i')
+                     Console.WriteLine("Not an i.");
+                 else
+                 {
+                     Console.WriteLine(letter);
+                 }
+             }
+
+            foreach (char letter in name)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+
+         }
+
+    }
+}
